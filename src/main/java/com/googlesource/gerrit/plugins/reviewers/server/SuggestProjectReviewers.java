1c9a67a4b16448946f8d0a0daf59ad891dcf41f0
Dave Borowitz <dborowitz@google.com>
2018-06-08 10:19:13 -0400

    Split reviewers plugin into GWT and non-GWT variants
    The UI portions have not yet been ported to PolyGerrit, but the plugin
    is still useful with no UI, particularly for auto-adding reviewers.
    
    Split the Module file into a ClientModule and BackendModule, and leave
    Module as simple wrapper that delegates to both of them. The
    backend-only plugin's code lives entirely in the server directory, and
    is factored out into a new plugin named reviewers-backend.
    
    The combined Module, its gwt.xml file, as well as its GWT-specific code
    (e.g. the top menu) must live directly above the "client" directory,
    otherwise the GWT build fails with a mysterious error.
    
    The gerrit_plugin rule needs to be tweaked slightly to allow multiple
    rules in the same BUILD file, not all of which match the directory name.
    
    Change-Id: I9e97987428c6e47b1f750ea05e01715ccb400caf

--- src/main/java/com/googlesource/gerrit/plugins/reviewers/server/SuggestProjectReviewers.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewers/server/SuggestProjectReviewers.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.googlesource.gerrit.plugins.reviewers;
+package com.googlesource.gerrit.plugins.reviewers.server;
 
 import com.google.gerrit.extensions.common.AccountVisibility;
 import com.google.gerrit.extensions.common.SuggestedReviewerInfo;
