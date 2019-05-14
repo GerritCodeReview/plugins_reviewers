58474af323b30aaa5dad784ea7a4eb3396bd8f37
Sven Selberg <svense@axis.com>
2019-05-14 16:00:45 +0200

    Polygerrit UI
    Bug: Issue 8351
    Change-Id: I1fa2f03f95e3b1b4217ec7a418931f5e3fdcf7cf

---
+++ gr-reviewers/rv-edit-screen.js
@@ -0,0 +1,58 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  Polymer({
+    is: 'rv-edit-screen',
+
+    properties: {
+      plugin: Object,
+      repoName: String,
+      canModifyConfig: Boolean,
+      _filterSections: Array,
+    },
+
+    attached: function() {
+      this._loadFilterSections();
+    },
+
+    _loadFilterSections() {
+      this.plugin.restApi().get(this._getReviewersUrl(this.repoName))
+        .then(filterSections => {
+          this._filterSections = filterSections;
+        })
+    },
+
+    _computeAddFilterBtnHidden(canModifyConfig) {
+      return !this.canModifyConfig;
+    },
+
+    _getReviewersUrl(repoName) {
+      return `/projects/${repoName}/reviewers`;
+    },
+
+    _handleCreateSection() {
+      const section = {filter: '', reviewers: [], editing: true};
+      this.push('_filterSections', section);
+    },
+
+    _handleCloseTap(e) {
+      e.preventDefault();
+      this.fire('close', null, {bubbles: false});
+    },
+
+    _handleReviewerChanged(e) {
+      this._filterSections = e.detail.result;
+    },
+  });
+})();
