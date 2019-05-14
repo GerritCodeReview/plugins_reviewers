1431b29c133517329d9dc303c2a8a732b1dc961a
Sven Selberg <svense@axis.com>
2019-05-14 16:00:45 +0200

    Polygerrit UI
    Bug: Issue 8351
    Change-Id: I1fa2f03f95e3b1b4217ec7a418931f5e3fdcf7cf

---
+++ gr-reviewers/rv-reviewer.js
@@ -0,0 +1,63 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  Polymer({
+    is : "rv-reviewer",
+
+    properties: {
+      reviewer: String,
+      _originalReviewer: String,
+      _deleted: Boolean,
+      _editing: {
+        type: Boolean,
+        computed: '_computeEditing(reviewer, _originalReviewer)'
+      }
+    },
+
+    attached() {
+      this._originalReviewer = this.reviewer;
+    },
+
+    _computeReviewerDisabled(reviewer, _originalReviewer) {
+      return !this._computeEditing(reviewer, this._originalReviewer);
+    },
+
+    _computeEditing(reviewer, _originalReviewer) {
+      if (_originalReviewer === '') {
+        return true;
+      }
+      return reviewer === '';
+    },
+
+    _getDeleteCancel(reviewer, _originalReviewer) {
+      return this._computeEditing(reviewer, _originalReviewer) ? 'Cancel' : 'Delete';
+    },
+
+    _getHideAddButton(reviewer, _originalReviewer) {
+      return !(this._computeEditing(reviewer, _originalReviewer) && this.$.editReviewerInput.value)
+    },
+
+    _handleDeleteCancel() {
+      const detail = {editing: this._editing}
+      this.dispatchEvent(new CustomEvent('reviewer-deleted', {detail, bubbles: true}));
+      this.remove();
+    },
+
+    _handleAddReviewer() {
+      const detail = {reviewer: this.reviewer};
+      this._originalReviewer = this.reviewer;
+      this.dispatchEvent(new CustomEvent('reviewer-added', {detail, bubbles: true}));
+    },
+  });
+})();
