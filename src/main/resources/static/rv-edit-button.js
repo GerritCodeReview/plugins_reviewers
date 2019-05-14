073190b14832c4bc27e8fd3286e3007e158c1a86
Sven Selberg <svense@axis.com>
2019-05-14 16:00:45 +0200

    Polygerrit UI
    Change-Id: I1fa2f03f95e3b1b4217ec7a418931f5e3fdcf7cf

---
+++ src/main/resources/static/rv-edit-button.js
@@ -0,0 +1,11 @@
+(function() {
+    Polymer({
+      is: 'rv-edit-button',
+      attached() {
+        console.log("I'm attached!")
+      },
+      _handleCommandTap() {
+        alert('Not implemented');
+      },
+    });
+})();
