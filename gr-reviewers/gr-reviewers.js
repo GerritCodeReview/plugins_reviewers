b5a15a6041f2022d601396e63c651f87286333cf
Sven Selberg <svense@axis.com>
2019-05-14 16:00:45 +0200

    Polygerrit UI
    Bug: Issue 8351
    Change-Id: I1fa2f03f95e3b1b4217ec7a418931f5e3fdcf7cf

---
+++ gr-reviewers/gr-reviewers.js
@@ -0,0 +1,74 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function() {
+  Polymer({
+    is: 'gr-reviewers',
+
+    properties: {
+      plugin: Object,
+      repoName: String,
+      _canModifyConfig: {
+        type: Boolean,
+        computed: '_computeCanModifyConfig(_isOwner, _hasModifyCapability)',
+      },
+      _isOwner: {
+        type: Boolean,
+        value: false,
+      },
+      _hasModifyCapability: {
+        type: Boolean,
+        value: false,
+      }
+    },
+
+    attached() {
+      this._setCanModifyConfig();
+    },
+
+    _handleCommandTap() {
+      this.$.rvScreenOverlay.open();
+    },
+
+    _handleRvEditScreenClose() {
+      this.$.rvScreenOverlay.close();
+    },
+
+    _setCanModifyConfig() {
+      if (this.repoName) {
+        this._getRepoAccess(this.repoName).then( access => {
+          if (access && access[this.repoName] && access[this.repoName].is_owner) {
+            this._isOwner = true;
+          }
+        })
+      }
+      this._getCapabilities().then(capabilities => {
+        if (capabilities['reviewers-modifyReviewersConfig']) {
+          this._hasModifyCapability = true;
+        }
+      })
+    },
+
+    _computeCanModifyConfig(isOwner, hasModifyCapability) {
+      return isOwner || hasModifyCapability;
+    },
+
+    _getRepoAccess(repoName) {
+      return this.plugin.restApi().get('/access/?project=' + encodeURIComponent(repoName));
+    },
+
+    _getCapabilities() {
+      return this.plugin.restApi().get('/accounts/self/capabilities');
+    }
+  });
+})();
