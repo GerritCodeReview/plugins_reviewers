f2cb7fda6682135adc8b3d0e7c072bbc2e72dec7
Sven Selberg <svense@axis.com>
2020-04-24 15:34:04 +0200

    Backend: Support cc
    Support for cc in:
    * Config
    * REST
    * Adding ccs on patchset upload
    
    Maintain backwards compatibility by defaulting the newly introduced
    ConfigReviewersInput#type to "REVIEWERS".
    
    TODO:
    Frontend support for cc.
    
    Feature: Issue 11676
    Change-Id: I2c2aba662f1a45ed4f2540b7eb32ee56cc8faaba

--- src/main/java/com/googlesource/gerrit/plugins/reviewers/config/ReviewersConfig.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewers/config/ReviewersConfig.java
@@ -26,6 +26,7 @@ import com.google.gerrit.server.project.NoSuchProjectException;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.reviewers.ReviewerFilter;
+import com.googlesource.gerrit.plugins.reviewers.ReviewerType;
 import java.io.IOException;
 import java.util.List;
 import org.eclipse.jgit.errors.ConfigInvalidException;
@@ -39,6 +40,7 @@ public class ReviewersConfig {
 
   @VisibleForTesting public static final String FILENAME = "reviewers.config";
   @VisibleForTesting public static final String SECTION_FILTER = "filter";
+  @VisibleForTesting public static final String KEY_CC = "cc";
   @VisibleForTesting public static final String KEY_REVIEWER = "reviewer";
   private static final String KEY_ENABLE_REST = "enableREST";
   private static final String KEY_SUGGEST_ONLY = "suggestOnly";
@@ -88,12 +90,24 @@ public class ReviewersConfig {
     private Config cfg;
     private ReviewerFilterCollection filters;
 
-    public void addReviewer(String filter, String reviewer) {
-      filters.get(filter).addReviewer(reviewer);
+    public void addReviewer(String filter, String reviewer, ReviewerType type) {
+      switch (type) {
+        case REVIEWER:
+          filters.get(filter).addReviewer(reviewer);
+          break;
+        case CC:
+          filters.get(filter).addCc(reviewer);
+      }
     }
 
-    public void removeReviewer(String filter, String reviewer) {
-      filters.get(filter).removeReviewer(reviewer);
+    public void removeReviewer(String filter, String reviewer, ReviewerType type) {
+      switch (type) {
+        case REVIEWER:
+          filters.get(filter).removeReviewer(reviewer);
+          break;
+        case CC:
+          filters.get(filter).removeCc(reviewer);
+      }
     }
 
     @Override
