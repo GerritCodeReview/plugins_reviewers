f6dc59abf56d51b7cb8c0ec725fc6d488eb63eb5
Sven Selberg <svense@axis.com>
2020-04-30 12:37:41 +0200

    Move enum Action into PutReviewers
    It is only used locally in PutReviewers in the internal class
    PutReviewers.Input.
    
    Change-Id: I66c9b981788ce1f672485362364d28f915905a77

--- src/main/java/com/googlesource/gerrit/plugins/reviewers/PutReviewers.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewers/PutReviewers.java
@@ -56,6 +56,11 @@ class PutReviewers implements RestModifyView<ProjectResource, Input> {
     public String reviewer;
   }
 
+  public enum Action {
+    ADD,
+    REMOVE
+  }
+
   private final String pluginName;
   private final ReviewersConfig config;
   private final Provider<MetaDataUpdate.User> metaDataUpdateFactory;
