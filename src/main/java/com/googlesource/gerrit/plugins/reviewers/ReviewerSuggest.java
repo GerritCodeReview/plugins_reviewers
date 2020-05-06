60dd631e53fc7d4b083a1dab3e35b0096c570e10
Sven Selberg <svense@axis.com>
2020-05-06 17:04:55 +0200

    Break out reviewer suggestions from Reviewer
    To separate suggestions from event handling.
    
    Feature: Issue 12694
    Change-Id: I2a842fbb62fa0d8d9a37bd2a6f92dbc92a985eda

---
+++ src/main/java/com/googlesource/gerrit/plugins/reviewers/ReviewerSuggest.java
@@ -0,0 +1,81 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.reviewers;
+
+import static java.util.stream.Collectors.toSet;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.entities.Change.Id;
+import com.google.gerrit.entities.Project.NameKey;
+import com.google.gerrit.exceptions.StorageException;
+import com.google.gerrit.index.query.QueryParseException;
+import com.google.gerrit.server.change.ReviewerSuggestion;
+import com.google.gerrit.server.change.SuggestedReviewer;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.reviewers.config.ReviewersConfig;
+import java.util.List;
+import java.util.Set;
+
+@Singleton
+public class ReviewerSuggest implements ReviewerSuggestion {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final ReviewersConfig config;
+  private final ReviewersFilterUtil util;
+  private final ReviewersResolver resolver;
+
+  @Inject
+  public ReviewerSuggest(
+      ReviewersConfig config, ReviewersFilterUtil filterUtil, ReviewersResolver resolver) {
+    this.config = config;
+    this.util = filterUtil;
+    this.resolver = resolver;
+  }
+
+  @Override
+  public Set<SuggestedReviewer> suggestReviewers(
+      NameKey project,
+      Id changeId,
+      String query,
+      Set<com.google.gerrit.entities.Account.Id> candidates) {
+    List<ReviewerFilter> sections = config.filtersWithInheritance(project);
+
+    if (sections.isEmpty() || changeId == null) {
+      return ImmutableSet.of();
+    }
+
+    try {
+      Set<String> reviewers = util.findReviewers(changeId.get(), sections);
+      if (!reviewers.isEmpty()) {
+        return resolver.resolve(reviewers, project, changeId.get(), null).stream()
+            .map(a -> suggestedReviewer(a))
+            .collect(toSet());
+      }
+    } catch (StorageException | QueryParseException x) {
+      logger.atSevere().withCause(x).log(x.getMessage());
+    }
+    return ImmutableSet.of();
+  }
+
+  private SuggestedReviewer suggestedReviewer(Account.Id account) {
+    SuggestedReviewer reviewer = new SuggestedReviewer();
+    reviewer.account = account;
+    reviewer.score = 1;
+    return reviewer;
+  }
+}
