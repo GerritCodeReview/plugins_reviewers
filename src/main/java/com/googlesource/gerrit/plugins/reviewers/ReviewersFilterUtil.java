8e0e4f4aefdb8fe6e20cf4c88efc87d698bad792
Sven Selberg <svense@axis.com>
2020-05-06 16:38:17 +0200

    Break out ReviewerFilter filtering to util class
    In preparation of separating event handling from suggests feature.
    
    Feature: Issue 12694
    Change-Id: Iefe1b4c6c6574cd2ebd160e73ec18500589eaa6b

---
+++ src/main/java/com/googlesource/gerrit/plugins/reviewers/ReviewersFilterUtil.java
@@ -0,0 +1,65 @@
+package com.googlesource.gerrit.plugins.reviewers;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.gerrit.exceptions.StorageException;
+import com.google.gerrit.index.query.QueryParseException;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.query.change.ChangeQueryBuilder;
+import com.google.gerrit.server.query.change.InternalChangeQuery;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import java.util.List;
+import java.util.Set;
+
+public class ReviewersFilterUtil {
+  private final ChangeQueryBuilder queryBuilder;
+  private final Provider<InternalChangeQuery> queryProvider;
+  private final Provider<CurrentUser> user;
+
+  @Inject
+  public ReviewersFilterUtil(
+      ChangeQueryBuilder queryBuilder,
+      Provider<InternalChangeQuery> queryProvider,
+      Provider<CurrentUser> user) {
+    this.queryBuilder = queryBuilder;
+    this.queryProvider = queryProvider;
+    this.user = user;
+  }
+
+  public Set<String> findReviewers(int change, List<ReviewerFilterSection> sections)
+      throws StorageException, QueryParseException {
+    ImmutableSet.Builder<String> reviewers = ImmutableSet.builder();
+    List<ReviewerFilterSection> found = findReviewerSections(change, sections);
+    for (ReviewerFilterSection s : found) {
+      reviewers.addAll(s.getReviewers());
+    }
+    return reviewers.build();
+  }
+
+  private List<ReviewerFilterSection> findReviewerSections(
+      int change, List<ReviewerFilterSection> sections)
+      throws StorageException, QueryParseException {
+    ImmutableList.Builder<ReviewerFilterSection> found = ImmutableList.builder();
+    for (ReviewerFilterSection s : sections) {
+      if (Strings.isNullOrEmpty(s.getFilter()) || s.getFilter().equals("*")) {
+        found.add(s);
+      } else if (filterMatch(change, s.getFilter())) {
+        found.add(s);
+      }
+    }
+    return found.build();
+  }
+
+  boolean filterMatch(int change, String filter) throws StorageException, QueryParseException {
+    Preconditions.checkNotNull(filter);
+    ChangeQueryBuilder qb = queryBuilder.asUser(user.get());
+    return !queryProvider
+        .get()
+        .noFields()
+        .query(qb.parse(String.format("change:%s %s", change, filter)))
+        .isEmpty();
+  }
+}
