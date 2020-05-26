55c2435faecf648e2ab630bf2ef2916997044d19
Sven Selberg <svense@axis.com>
2020-05-26 17:16:19 +0200

    Validate reviewer filters
    REST:
    * Validate reviewer filters before adding them to reviewers.config.
    git:
    * Validate reviewer filters before update of reviewers.config.
    
    Introduce a query validation API.
    
    Feature: Issue 12650
    Change-Id: I1404598869ccf15714063a088e1410cc8ba5274a

--- src/main/java/com/googlesource/gerrit/plugins/reviewers/config/ForProject.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewers/config/ForProject.java
@@ -17,21 +17,37 @@ package com.googlesource.gerrit.plugins.reviewers.config;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Strings;
 import com.google.gerrit.entities.RefNames;
+import com.google.gerrit.server.git.ValidationError;
 import com.google.gerrit.server.git.meta.VersionedMetaData;
+import com.google.inject.Inject;
 import com.googlesource.gerrit.plugins.reviewers.ReviewerType;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.lib.CommitBuilder;
 import org.eclipse.jgit.lib.Config;
 
-public class ForProject extends VersionedMetaData {
+public class ForProject extends VersionedMetaData implements ValidationError.Sink {
   @VisibleForTesting public static final String FILENAME = "reviewers.config";
   @VisibleForTesting public static final String SECTION_FILTER = "filter";
   @VisibleForTesting public static final String KEY_CC = "cc";
   @VisibleForTesting public static final String KEY_REVIEWER = "reviewer";
 
+  public interface Factory {
+    public ForProject create();
+  }
+
+  private final ReviewerFilterCollection.Factory filterCollectionFactory;
   private Config cfg;
   private ReviewerFilterCollection filters;
+  private List<ValidationError> validationErrors;
+
+  @Inject
+  public ForProject(ReviewerFilterCollection.Factory filterCollectionFactory) {
+    this.filterCollectionFactory = filterCollectionFactory;
+  }
 
   public void addReviewer(String filter, String reviewer, ReviewerType type) {
     switch (type) {
@@ -53,6 +69,26 @@ public class ForProject extends VersionedMetaData {
     }
   }
 
+  /**
+   * Get the validation errors, if any were discovered.
+   *
+   * @return list of errors; empty list if there are no errors.
+   */
+  public List<ValidationError> getValidationErrors() {
+    if (validationErrors != null) {
+      return Collections.unmodifiableList(validationErrors);
+    }
+    return Collections.emptyList();
+  }
+
+  @Override
+  public void error(ValidationError error) {
+    if (validationErrors == null) {
+      validationErrors = new ArrayList<>(4);
+    }
+    validationErrors.add(error);
+  }
+
   @Override
   protected String getRefName() {
     return RefNames.REFS_CONFIG;
@@ -61,7 +97,7 @@ public class ForProject extends VersionedMetaData {
   @Override
   protected void onLoad() throws IOException, ConfigInvalidException {
     this.cfg = readConfig(FILENAME);
-    this.filters = new ReviewerFilterCollection(cfg);
+    this.filters = filterCollectionFactory.create(cfg, this);
   }
 
   @Override
