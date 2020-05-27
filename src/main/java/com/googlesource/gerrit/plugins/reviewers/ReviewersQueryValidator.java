94ac9591a2ef7e22a8e91a5fdd4c3d42f8d95ff3
Sven Selberg <svense@axis.com>
2020-05-27 12:23:44 +0200

    Don't query index when checking if change matches filter
    This change limits the set of supported operands in a reviewer filter
    query to those that do not rely on the index.
    
    Since predicates currently doesn't know if they are depending on the
    index or not we will have to maintain a static list of supported
    query operators. This list is also unfortunately duplicated between
    code and Documentation.
    
    The checks plugin faces similar challenges and have already come
    up with a solution for them, much of the validation of supported
    operators and there enumeration are copied from there:
    https://gerrit.googlesource.com/plugins/checks/+/ \
      08d8aad35e27f07f50095037bd99b173d8d7588e/ \
      java/com/google/gerrit/plugins/checks/CheckerQuery.java
    
    This results in additional duplication until predicates can say
    if they are index-dependent or not.
    
    Feature: Issue 12610
    Change-Id: I84bb11cd82d5f1c7539a96da2ec640b7a858fae6

--- src/main/java/com/googlesource/gerrit/plugins/reviewers/ReviewersQueryValidator.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewers/ReviewersQueryValidator.java
@@ -14,27 +14,117 @@
 
 package com.googlesource.gerrit.plugins.reviewers;
 
+import static com.google.gerrit.index.query.QueryParser.AND;
+import static com.google.gerrit.index.query.QueryParser.DEFAULT_FIELD;
+import static com.google.gerrit.index.query.QueryParser.FIELD_NAME;
+import static com.google.gerrit.index.query.QueryParser.NOT;
+import static com.google.gerrit.index.query.QueryParser.OR;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableSortedSet;
 import com.google.gerrit.index.query.QueryParseException;
-import com.google.gerrit.server.query.change.ChangeQueryBuilder;
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
+import com.google.gerrit.index.query.QueryParser;
+import org.antlr.runtime.tree.Tree;
 
 /** Validates that a reviewer filter query is formatted correctly. */
-@Singleton
-public class ReviewersQueryValidator {
-  private final ChangeQueryBuilder queryBuilder;
+public abstract class ReviewersQueryValidator {
 
-  @Inject
-  public ReviewersQueryValidator(ChangeQueryBuilder queryBuilder) {
-    this.queryBuilder = queryBuilder;
-  }
+  // Since predicates doesn't currently know if they are supported on non-index queries, this
+  // set is a copy-paste from CheckerQuery.java in the checks plugin:
+  // https://gerrit.googlesource.com/plugins/checks/+/08d8aad35e27f07f50095037bd99b173d8d7588e \
+  // /java/com/google/gerrit/plugins/checks/CheckerQuery.java
+  //
+  // Note that this list contains *operators*, not predicates. If there are multiple operators
+  // aliased together to the same predicate ("f:", "file:"), they all need to be listed explicitly.
+  //
+  // We chose to list operators instead of predicates because:
+  //  * It insulates us from changes in implementation details of the query system, such as
+  //    predicate classes being renamed, or additional predicates being ORed into existing
+  //    operators.
+  //  * It's easier to keep in sync with the documentation.
+  //
+  // This doesn't rule out switching to predicates in the future, particularly if the predicate
+  // classes gain some informative methods like "boolean matchMethodNeedsToQueryIndex()".
+  //
+  // Predicates that definitely cannot be allowed:
+  //  * Anything where match() needs to query the index, i.e. any full-text fields. This includes
+  //    the default field.
+  //  * Anything where post-filtering is unreasonably expensive.
+  //  * Any predicate over projects, since that may conflict with the projects field.
+  //
+  // Beyond that, this set is mostly based on what we subjectively consider useful for limiting the
+  // changes that a checker runs on. It will probably grow, based on user feedback.
+  private static final ImmutableSortedSet<String> ALLOWED_OPERATORS =
+      ImmutableSortedSet.of(
+          "added",
+          "after",
+          "age",
+          "assignee",
+          "author",
+          "before",
+          "branch",
+          "committer",
+          "deleted",
+          "delta",
+          "destination",
+          "dir",
+          "directory",
+          "ext",
+          "extension",
+          "f",
+          "file",
+          "footer",
+          "hashtag",
+          "intopic",
+          "label",
+          "onlyextensions",
+          "onlyexts",
+          "ownerin",
+          "path",
+          "r",
+          "ref",
+          "reviewer",
+          "reviewerin",
+          "size",
+          "status",
+          "submittable",
+          "topic",
+          "unresolved",
+          "wip");
 
-  void validateQuery(String query) throws UnsupportedReviewersQueryException {
+  static void validateQuery(String query) throws UnsupportedReviewersQueryException {
+    String trimmedQuery = requireNonNull(query, "Query must not be null").trim();
+    /* Reviewers supports '*' as a special case query. */
+    if (trimmedQuery.equals("*")) {
+      return;
+    }
     try {
-      queryBuilder.parse(query);
+      validateOperators(QueryParser.parse(trimmedQuery));
     } catch (QueryParseException e) {
       throw new UnsupportedReviewersQueryException(
           String.format("Unsupported filter: \"%s\"", query), e);
+    }
+  }
+
+  private static void validateOperators(Tree node) throws UnsupportedReviewersQueryException {
+    switch (node.getType()) {
+      case AND:
+      case OR:
+      case NOT:
+        for (int i = 0; i < node.getChildCount(); i++) {
+          validateOperators(node.getChild(i));
+        }
+        break;
+      case FIELD_NAME:
+        if (!ALLOWED_OPERATORS.contains(node.getText())) {
+          throw new UnsupportedReviewersQueryException("Unsupported operator: " + node);
+        }
+        break;
+      case DEFAULT_FIELD:
+        throw new UnsupportedReviewersQueryException(
+            "Specific search operator required: " + node.getChild(0));
+      default:
+        throw new UnsupportedReviewersQueryException("Unsupported filter: " + node);
     }
   }
 }
